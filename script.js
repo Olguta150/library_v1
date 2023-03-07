ed707e0c5184244c77d61904f1d0e3afc7b01d5c
Olguta Fratila <fratilaolguta150@gmail.com>
2023-03-07 13:08:01 +0200

    create a book object

---
+++ script.js
@@ -0,0 +1,16 @@
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+
+console.log(theHobbit.info());
